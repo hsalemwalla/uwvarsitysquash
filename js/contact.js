e1789eabbe7e12bf301fde4d1e1b3fb18f277680
hsalemwalla <hsalemwalla@gmail.com>
2014-10-06 12:33:23 -0400

    Removed unused pages and created ones for initial release

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+function generateMap() {
+    var frame = '<iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1447.7153363984758!2d-80.54630321622082!3d43.472481849076736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x64b17eee7cfcdcde!2sPhysical+Activities+Complex!5e0!3m2!1sen!2sca!4v1412611095291" width="600" height="450" frameborder="0" style="border:0"></iframe>';
+    document.getElementById("googlemap").innerHTML = frame;
+}
