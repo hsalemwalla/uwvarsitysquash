fd5e96d70c392f70b203b852f143c635de49f36f
hsalemwalla <hsalemwalla@gmail.com>
2017-07-14 11:06:11 -0400

    removed index old and updated nav bar

--- js/common.js
+++ js/common.js
@@ -13,7 +13,7 @@ function generateNavigationBar() {
     navBar += '<span class="icon-bar"></span>';
     navBar += '<span class="icon-bar"></span>';
     navBar += '</button>';
-    navBar += '<a class="navbar-brand navBarText" href="index_old.html">Home</a>';
+    navBar += '<a class="navbar-brand navBarText" href="index.html">Home</a>';
     navBar += '</div>';
     navBar += '<!-- Collect the nav links, forms, and other content for toggling -->';
     navBar += '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
@@ -36,44 +36,6 @@ function generateNavigationBar() {
     navBar += '<li>';
     navBar += '<a href="drills.html" class="navBarText">Drills</a>';
     navBar += '</li>';
-    
-
-    /*
-     *navBar += '<li class="dropdown">';
-     *navBar += '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>';
-     *navBar += '<ul class="dropdown-menu">';
-     *navBar += '<li>';
-     *navBar += '<a href="blog-home-1.html">Blog Home 1</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="blog-home-2.html">Blog Home 2</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="blog-post.html">Blog Post</a>';
-     *navBar += '</li>';
-     *navBar += '</ul>';
-     *navBar += '</li>';
-     *navBar += '<li class="dropdown">';
-     *navBar += '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>';
-     *navBar += '<ul class="dropdown-menu">';
-     *navBar += '<li>';
-     *navBar += '<a href="full-width.html">Full Width Page</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="sidebar.html">Sidebar Page</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="faq.html">FAQ</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="404.html">404</a>';
-     *navBar += '</li>';
-     *navBar += '<li>';
-     *navBar += '<a href="pricing.html">Pricing Table</a>';
-     *navBar += '</li>';
-     *navBar += '</ul>';
-     *navBar += '</li>';
-     */
     navBar += '</ul>';
     navBar += '</div>';
     navBar += '<!-- /.navbar-collapse -->';
